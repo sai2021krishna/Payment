00670fdfcb67a25f3965b6486173ba703e04295e
kanha <saikrishna.at2021@gmail.com>
2022-11-02 14:35:36 +0530

    hello world

--- src/main/java/com/capstoneproject/payment/PaymentApplication.java
+++ src/main/java/com/capstoneproject/payment/PaymentApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentApplication {
 
+	//hello
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
 	}
