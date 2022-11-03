8df865ffdcc6e3fe52db9901ad1850026dc7f814
kanha <saikrishna.at2021@gmail.com>
2022-11-03 11:55:38 +0530

    test cases modified Mokito Added - to be verified by Himanshu

--- Barclays_Payments_Project/src/test/java/com/barclays/BarclaysPaymentsProjectTests.java
+++ Barclays_Payments_Project/src/test/java/com/barclays/BarclaysPaymentsProjectTests.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.barclays.dto.UserDTO;
 import com.barclays.entity.User;
 import com.barclays.exception.PaymentsException;
 import com.barclays.repository.UserRespository;
@@ -29,15 +30,17 @@ class BarclaysPaymentsProjectTests {
 		User User = new User();
 		User.setLoginId(1000001);
 		User.setPassword("123");
+		ResponseEntity<String> resp=new ResponseEntity<>("User logged in Successfully with User id: User [ loginId= "+User.getLoginId().shortValue()+"password=" +User.getPassword()+"]", HttpStatus.OK);
+		Mockito.when(userService.loginUser(User)).thenReturn(resp);
+		//("User added Successfully with User id: User [ loginId= "+User.getLoginId().shortValue()+"password=" +User.getPassword()+"]")
 		ResponseEntity<String> actual = userService.loginUser(User);
-		Assertions.assertEquals("User successfully logged in :1000001ASAccount_Holder", actual);
+		Assertions.assertEquals("User logged in Successfully with User id: User [ loginId= "+User.getLoginId().shortValue()+"password=" +User.getPassword()+"]", actual);
 	} 
 	@Test
 	public void authenticateUserTestInValidCredentials() throws PaymentsException {
 		User User = new User();
 		User.setLoginId(1000001);
 		User.setPassword("1");
-		
 	    PaymentsException exception=Assertions.assertThrows(PaymentsException.class,()->userService.loginUser(User));
 		Assertions.assertEquals("Service.INCORRECT", exception.getMessage());
 	}
